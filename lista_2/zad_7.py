357df6e013b37485b90922a467309ea8d75fd21c
xywa5000 <xywa5000@gmail.com>
2024-05-19 16:34:40 +0200

    List 2
    Ex. 7 final

--- lista_2/zad_7.py
+++ lista_2/zad_7.py
@@ -4,10 +4,15 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from python_lib.Polynomial import Polynomial
 
-# Przykładowe użycie funkcji
 a = Polynomial([1., 0., 1., 0., 1.])
 b = Polynomial([-1., -2., -1., 0., 1.])
 c = Polynomial([-1., 0., 0., 1])
 d = Polynomial([-4., -4., 1., 1.])
 e = Polynomial([4., -4., -1., 1.])
 f = Polynomial([2., -1., -2., 1.])
+
+print(f"GCD: {a}; {b}; {c}")
+print(Polynomial.gcd_all([a, b, c]))
+
+print(f"GCD: {d}; {e}; {f}")
+print(Polynomial.gcd_all([d, e, f]))
