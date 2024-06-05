a877a67635c7719e11561bb1f6832fe5c344668d
xywa5000 <xywa5000@gmail.com>
2024-06-05 18:23:15 +0200

    List 2
    Fixed  zad_7.py, added comments

--- lista_2/zad_7.py
+++ lista_2/zad_7.py
@@ -4,7 +4,6 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from python_lib.Polynomial import Polynomial
 
-# TODO Dodac dzielenie wyniku przez największy wspólny dzielnik
 
 a = Polynomial([1., 0., 1., 0., 1.])
 b = Polynomial([-1., -2., -1., 0., 1.])
@@ -13,8 +12,12 @@ d = Polynomial([-4., -4., 1., 1.])
 e = Polynomial([4., -4., -1., 1.])
 f = Polynomial([2., -1., -2., 1.])
 
+# wolframalpha:
+# gcd([//math:x^4+x^2+1//], [//math:x^4-x^2-2x-1//], [//math:x^3-1//])
 print(f"GCD: {a}; {b}; {c}")
 print(Polynomial.gcd_all([a, b, c]))
 
+# wolframalpha:
+# gcd([//math:x^3+x^2-4x-4//], [//math:x^3-x^2-4x+4//], [//math:x^3-2x^2-x+2//])
 print(f"GCD: {d}; {e}; {f}")
 print(Polynomial.gcd_all([d, e, f]))
